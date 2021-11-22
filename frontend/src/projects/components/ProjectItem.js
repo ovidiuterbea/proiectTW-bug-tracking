3a472298640de1f224839c237e367373cd7ed3b9
ovidiuterbea <ovidiuminecraft1234@gmail.com>
2021-11-22 11:53:05 +0200

    added some styling

--- frontend/src/projects/components/ProjectItem.js
+++ frontend/src/projects/components/ProjectItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./ProjectItem.css";
+import { Link } from "react-router-dom";
 
 const ProjectItem = (props) => {
   return (
@@ -11,8 +12,16 @@ const ProjectItem = (props) => {
         <a href={props.repo}>
           <button>Go to repo</button>
         </a>
-        {props.isTester && <button>Buton catre creeare bug</button>}
-        {!props.isTester && <button>Buton catre vizualizare proiect</button>}
+        {props.isTester && (
+          <Link>
+            <button>Buton catre creeare bug</button>
+          </Link>
+        )}
+        {!props.isTester && (
+          <Link>
+            <button>Buton catre vizualizare proiect</button>
+          </Link>
+        )}
       </div>
     </li>
   );
