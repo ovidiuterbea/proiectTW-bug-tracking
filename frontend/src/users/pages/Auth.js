182cc5bf592dd27682ac9e59bc98ad163c4476a2
Bogdan Udris <bogdan@Bogdans-MacBook-Pro.local>
2021-11-23 12:52:00 +0200

    Added Authentication

--- frontend/src/users/pages/Auth.js
+++ frontend/src/users/pages/Auth.js
@@ -1,7 +1,37 @@
 import React from "react";
+import "./Auth.css";
+import { Button, Card, CardContent, Grid, TextField, Typography } from "@mui/material";
 
 const Auth = () => {
-  return <h3>Aceasta este pagina de autenficare!</h3>;
+  return (
+    <div className="App"> 
+      <Typography padding="1rem" variant="h3" align="center" color="#f3f3f3">
+        Log In
+       </Typography>
+      <Grid>
+        <Card className="mainCardContainer" style={{ backgroundColor:"#f3f3f3",borderRadius:"10px", maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}>
+          <CardContent> 
+            <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
+              Log In into the app using your email and password.
+          </Typography> 
+            <form>
+              <Grid container spacing={1}>
+                <Grid item xs={12} sm={6}>
+                  <TextField className="textField" type="email" placeholder="Enter email" label="Email" variant="outlined" fullWidth required />
+                </Grid>
+                <Grid xs={12} sm={6} item>
+                  <TextField className="textField" type="password" placeholder="Enter password" label="Password" variant="outlined" fullWidth required />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button className="muibtn" type="submit" variant="contained" color="primary" fullWidth>Submit</Button>
+                </Grid>
+              </Grid>
+            </form>
+          </CardContent>
+        </Card>
+      </Grid>
+    </div>
+  );
 };
 
 export default Auth;
