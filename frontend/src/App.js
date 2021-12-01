29bccd413e3cf3d2a46f5d942aa1cdff16337c69
ovidiuterbea <ovidiuminecraft1234@gmail.com>
2021-12-01 12:59:40 +0200

    addec context for the user authentication (navlinks different if is logged and different routes depending on the state of context)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,35 +12,63 @@ import UserProjects from "./projects/pages/UserProjects";
 import NavLinks from "./shared/components/Navigation/NavLinks";
 import ProjectBugs from "./bugs/pages/ProjectBugs";
 import BugDetails from "./bugs/pages/BugDetails";
+import { useState, useCallback } from "react";
+import { AuthContext } from "./shared/context/auth-context";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  let routes;
+
+  if (isLoggedIn) {
+    routes = (
+      <Switch>
+        <Route path='/projects/new' exact>
+          <NewProject />
+        </Route>
+        <Route path='/projects' exact>
+          <Projects />
+        </Route>
+        <Route path='/:userId/projects' exact>
+          <UserProjects />
+        </Route>
+        <Route path='/:userId/projects/:projectId/bugs' exact>
+          <ProjectBugs />
+        </Route>
+        <Route path='/:userId/projects/:projectId/bugs/:bugId' exact>
+          <BugDetails />
+        </Route>
+        <Redirect to='/' />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path='/' exact>
+          <Auth />
+        </Route>
+        <Redirect to='/auth' />
+      </Switch>
+    );
+  }
+
   return (
-    <React.Fragment>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
       <Router>
         <NavLinks />
-        <Switch>
-          <Route path='/' exact>
-            <Auth />
-          </Route>
-          <Route path='/projects/new' exact>
-            <NewProject />
-          </Route>
-          <Route path='/projects' exact>
-            <Projects />
-          </Route>
-          <Route path='/:userId/projects' exact>
-            <UserProjects />
-          </Route>
-          <Route path='/:userId/projects/:projectId/bugs' exact>
-            <ProjectBugs />
-          </Route>
-          <Route path='/:userId/projects/:projectId/bugs/:bugId' exact>
-            <BugDetails />
-          </Route>
-          <Redirect to='/' />
-        </Switch>
+        {routes}
       </Router>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
--- frontend/src/shared/components/Navigation/NavLinks.js
+++ frontend/src/shared/components/Navigation/NavLinks.js
@@ -1,40 +1,51 @@
-// import React, { useContext } from "react";
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 
-// import { AuthContext } from "../../context/auth-context";
+import { AuthContext } from "../../context/auth-context";
 import "./NavLinks.css";
 
 const NavLinks = (props) => {
-  //   const auth = useContext(AuthContext);
+  const auth = useContext(AuthContext);
 
   return (
     <div className='main-navigation'>
       <h3>Bug tracking </h3>
       <ul className='nav-links'>
-        <li>
-          <NavLink activeClassName='activeLink' to='/u1/projects'>
-            MY PROJECTS
-          </NavLink>
-        </li>
-        <li>
-          <NavLink activeClassName='activeLink' to='/projects' exact>
-            ALL PROJECTS
-          </NavLink>
-        </li>
-        <li>
-          <NavLink activeClassName='activeLink' to='/projects/new'>
-            NEW PROJECT
-          </NavLink>
-        </li>
+        {auth.isLoggedIn && (
+          <li>
+            <NavLink activeClassName='activeLink' to='/u1/projects'>
+              MY PROJECTS
+            </NavLink>
+          </li>
+        )}
+        {auth.isLoggedIn && (
+          <li>
+            <NavLink activeClassName='activeLink' to='/projects' exact>
+              ALL PROJECTS
+            </NavLink>
+          </li>
+        )}
+        {auth.isLoggedIn && (
+          <li>
+            <NavLink activeClassName='activeLink' to='/projects/new'>
+              NEW PROJECT
+            </NavLink>
+          </li>
+        )}
 
-        <li>
-          <NavLink to='/'>AUTHENTICATE</NavLink>
-        </li>
+        {!auth.isLoggedIn && (
+          <li>
+            <NavLink to='/'>AUTHENTICATE</NavLink>
+          </li>
+        )}
 
-        {/* <li>
-        <button onClick={auth.logout}>LOGOUT</button>
-      </li> */}
+        {auth.isLoggedIn && (
+          <li>
+            <NavLink onClick={auth.logout} to='/'>
+              LOGOUT
+            </NavLink>
+          </li>
+        )}
       </ul>
     </div>
   );
