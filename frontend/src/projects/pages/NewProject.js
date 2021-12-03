4bc1ea27675810454b630a8c782a38b4f6362517
ovidiuterbea <ovidiuminecraft1234@gmail.com>
2021-12-03 17:34:25 +0200

    added the add project routes to the application

--- frontend/src/projects/pages/NewProject.js
+++ frontend/src/projects/pages/NewProject.js
@@ -9,12 +9,43 @@ import {
   Autocomplete,
   Typography,
 } from "@mui/material";
+import { useEffect } from "react";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import Slide from "@mui/material/Slide";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction='up' ref={ref} {...props} />;
+});
 
 const NewProject = () => {
-  const [users, setUsers] = useState([]);
+  const [usersId, setUsersId] = useState([]);
   const [enteredUser, setEnteredUser] = useState("");
   const [enteredProjectName, setEnteredProjectName] = useState("");
   const [enteredRepo, setEnteredRepo] = useState("");
+  const { sendRequest } = useHttpClient();
+  const [loadedUsersFetch, setLoadedUsersFetch] = useState([]);
+  const [userId, setUserId] = useState("");
+  const [open, setOpen] = React.useState(false);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:8000/api/users/`
+        );
+        setLoadedUsersFetch(responseData.users);
+      } catch (err) {}
+    };
+    fetchUsers();
+  }, [sendRequest]);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const projectChangeHandler = (event) => {
     setEnteredProjectName(event.target.value);
@@ -29,112 +60,149 @@ const NewProject = () => {
   };
 
   const usersChangeHandler = (event) => {
-    setUsers((arr) => [...arr, enteredUser]);
-    console.log(users);
+    setUsersId((arr) => [...arr, userId]);
+    setOpen(true);
+    console.log(usersId);
   };
 
-  const data = [
-    { name: "Tudor Delia" },
-    { name: "Udris Bogdan" },
-    { name: "Terbea Ovidiu" },
-  ];
-
-  const formHandler = (event) => {
+  const formHandler = async (event) => {
     event.preventDefault();
 
     const newProject = {
       name: enteredProjectName,
       repo: enteredRepo,
-      users: users,
+      users: usersId,
     };
+
+    console.log(newProject);
+    try {
+      await sendRequest(
+        "http://localhost:8000/api/projects",
+        "POST",
+        JSON.stringify({
+          name: enteredProjectName,
+          repo: enteredRepo,
+          users: usersId,
+        }),
+        { "Content-Type": "application/json" }
+      );
+    } catch (err) {}
 
     setEnteredProjectName("");
     setEnteredRepo("");
-
-    console.log(newProject);
+    setEnteredUser("");
   };
 
   return (
-    <div className='App'>
-      <Typography padding='1rem' variant='h3' align='center' color='#f3f3f3'>
-        Add Project
-      </Typography>
-      <Grid>
-        <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}>
-          <CardContent>
-            <form onSubmit={formHandler}>
-              <Grid container spacing={1}>
-                <Grid xs={12} item>
-                  <TextField
-                    placeholder='Enter project name'
-                    label='Project Name'
-                    variant='outlined'
-                    fullWidth
-                    required
-                    onChange={projectChangeHandler}
-                    value={enteredProjectName}
-                  />
+    <React.Fragment>
+      {open && (
+        <Dialog
+          open={open}
+          TransitionComponent={Transition}
+          onClose={handleClose}
+          aria-describedby='alert-dialog-slide-description'
+        >
+          <DialogContent>
+            <DialogContentText id='alert-dialog-slide-description'>
+              {enteredUser} has been added to the project
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Ok</Button>
+          </DialogActions>
+        </Dialog>
+      )}
+      <div className='App'>
+        <Typography padding='1rem' variant='h3' align='center' color='#f3f3f3'>
+          Add Project
+        </Typography>
+        <Grid>
+          <Card
+            style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}
+          >
+            <CardContent>
+              <form onSubmit={formHandler}>
+                <Grid container spacing={1}>
+                  <Grid xs={12} item>
+                    <TextField
+                      placeholder='Enter project name'
+                      label='Project Name'
+                      variant='outlined'
+                      fullWidth
+                      required
+                      onChange={projectChangeHandler}
+                      value={enteredProjectName}
+                    />
+                  </Grid>
+                  <Grid xs={12} item>
+                    <TextField
+                      placeholder='Enter repository link'
+                      label='Repository'
+                      variant='outlined'
+                      fullWidth
+                      required
+                      onChange={repoChangeHandler}
+                      value={enteredRepo}
+                    />
+                  </Grid>
+                  <Grid xs={12} sm={8} item>
+                    <Autocomplete
+                      id='combo-box-existent-users'
+                      options={loadedUsersFetch}
+                      getOptionLabel={(option) =>
+                        option.name + " " + option.surname
+                      }
+                      isOptionEqualToValue={(option, value) =>
+                        option.id === value.id
+                      }
+                      renderInput={(params) => (
+                        <div ref={params.InputProps.ref}>
+                          <TextField
+                            inputProps={{ ...params.inputProps }}
+                            placeholder='Select users for this project'
+                            label='Users'
+                            variant='outlined'
+                            fullWidth
+                            onBlur={enteredUserChangeHandler}
+                            value={enteredUser}
+                          />
+                        </div>
+                      )}
+                      onChange={(event, newValue) => {
+                        if (newValue !== null) {
+                          setUserId(newValue.id);
+                        }
+                      }}
+                    />
+                  </Grid>
+                  <Grid item sm={4} xs={12}>
+                    <Button
+                      type='button'
+                      variant='contained'
+                      color='primary'
+                      fullWidth
+                      onClick={usersChangeHandler}
+                    >
+                      Add user
+                    </Button>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Button
+                      type='submit'
+                      variant='contained'
+                      color='primary'
+                      fullWidth
+                    >
+                      Submit
+                    </Button>
+                  </Grid>
                 </Grid>
-                <Grid xs={12} item>
-                  <TextField
-                    placeholder='Enter repository link'
-                    label='Repository'
-                    variant='outlined'
-                    fullWidth
-                    required
-                    onChange={repoChangeHandler}
-                    value={enteredRepo}
-                  />
-                </Grid>
-                <Grid xs={12} sm={8} item>
-                  <Autocomplete
-                    id='combo-box-existent-users'
-                    options={data}
-                    getOptionLabel={(option) => option.name}
-                    isOptionEqualToValue={(option, value) =>
-                      option.id === value.id
-                    }
-                    renderInput={(params) => (
-                      <div ref={params.InputProps.ref}>
-                        <TextField
-                          inputProps={{ ...params.inputProps }}
-                          placeholder='Select users for this project'
-                          label='Users'
-                          variant='outlined'
-                          fullWidth
-                          onBlur={enteredUserChangeHandler}
-                        />
-                      </div>
-                    )}
-                  />
-                </Grid>
-                <Grid item sm={4} xs={12}>
-                  <Button
-                    type='button'
-                    variant='contained'
-                    color='primary'
-                    fullWidth
-                    onClick={usersChangeHandler}
-                  >
-                    Add user
-                  </Button>
-                </Grid>
-                <Grid item xs={12}>
-                  <Button
-                    type='submit'
-                    variant='contained'
-                    color='primary'
-                    fullWidth
-                  >
-                    Submit
-                  </Button>
-                </Grid>
-              </Grid>
-            </form>
-          </CardContent>
-        </Card>
-      </Grid>
-    </div>
+              </form>
+            </CardContent>
+          </Card>
+        </Grid>
+      </div>
+    </React.Fragment>
   );
 };
 
