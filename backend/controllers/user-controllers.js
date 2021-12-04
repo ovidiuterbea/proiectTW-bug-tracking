81c542d091d8a52962d41e16bd105f672d31423b
ovidiuterbea <ovidiuminecraft1234@gmail.com>
2021-12-04 13:33:46 +0200

    added hashing to the passwords on the backend

--- backend/controllers/user-controllers.js
+++ backend/controllers/user-controllers.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
@@ -74,11 +75,19 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    const error = new HttpError("Could not create hash the password.", 500);
+    return next(error);
+  }
+
   const createdUser = new User({
     name,
     email,
     image: "./images/244531.png",
-    password,
+    password: hashedPassword,
     surname,
     projects: [],
   });
@@ -109,7 +118,22 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
-  if (!existingUser || existingUser.password !== password) {
+  if (!existingUser) {
+    const error = new HttpError(
+      "Invalid credentials, could not log you in.",
+      401
+    );
+    return next(error);
+  }
+
+  let isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+    const error = new HttpError("Could not log user in.", 500);
+  }
+
+  if (!isValidPassword) {
     const error = new HttpError(
       "Invalid credentials, could not log you in.",
       401
